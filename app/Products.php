d6308e05c8949ad48d25a0271f6ef0f68002a522
quach-duc-mulodo <quach.duc@mulodo.com>
2015-09-14 17:30:01 +0700

    Create model product for app

---
+++ app/Products.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+// use Eloquent (ORM of laravel) Read more: http://laravel.com/docs/5.0/eloquent
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    //
+}
