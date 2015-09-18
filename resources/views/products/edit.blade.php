fed02731c954e5e4db0937c798a7607b3f48b7f3
quach-duc-mulodo <quach.duc@mulodo.com>
2015-09-18 17:13:58 +0700

    Apply bootstrap style for UI

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -4,24 +4,34 @@
   <h1>Edit product</h1>
   {{-- NOTE: Use Form of html service --}}
   {{--       Read More: http://laravelcollective.com/docs/5.1/html --}}
-  {!! Form::model($pro,['method' => 'PATCH', 'action' => ['ProductsController@update', $pro->id]]) !!}
-  {!! Form::label('name','Name:') !!}
-  {!! Form::input('text', 'name') !!}
-  <br/>
-  <br/>
-  {!! Form::label('pro_code','Product code:') !!}
-  {!! Form::input('text', 'pro_code') !!}
-  <br/>
-  <br/>
-  {!! Form::label('desc','Product desc:') !!}
-  {!! Form::input('text', 'desc') !!}
-  <br/>
-  <br/>
-  {!! Form::label('created_at','Created date:') !!}
-  {{-- NOTE: date("Y-m-d") -> get current data for default value --}}
-  {!! Form::input('date', 'created_at', date("Y-m-d")) !!}
-  <br/>
-  <br/>
-  {!! Form::submit('Update') !!}
-  {!! Form::close() !!}
+  {!! Form::model($pro,['method' => 'PATCH', 'action' => ['ProductsController@update', $pro->id]], array('class' => 'col col-md-5')) !!}
+  <div class="col col-md-5">
+    <div class="form-group">
+      {!! Form::label('name','Name:') !!}
+      {!! Form::text('name', $pro->name, array('class' => 'form-control')) !!}
+    </div>
+    <br/>
+    <br/>
+    <div class="form-group">
+      {!! Form::label('pro_code','Product code:') !!}
+      {!! Form::text('pro_code', $pro->pro_code, array('class' => 'form-control')) !!}
+    </div>
+    <br/>
+    <br/>
+    <div class="form-group">
+      {!! Form::label('desc','Product desc:') !!}
+      {!! Form::text('desc', $pro->desc, array('class' => 'form-control')) !!}
+    </div>
+    <br/>
+    <br/>
+    <div class="form-group">
+      {!! Form::label('created_at','Created date:') !!}
+      {{-- NOTE: date("Y-m-d") -> get current data for default value --}}
+      {!! Form::input('date', 'created_at', date("Y-m-d"), array('class' => 'form-control')) !!}
+    </div>
+    <br/>
+    <br/>
+    {!! Form::submit('Update', array('class' => 'btn btn-success')) !!}
+    {!! Form::close() !!}
+  </div>
 @stop
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,13 +9,13 @@
         Code: {{$product->pro_code}} |
         Desc: {{$product->desc}} |
         Created at: {{$product->created_at}} |
-        <a href="/products/{{$product->id}}/edit">Edit</a> |
-        <a href="/products/{{$product->id}}">Show</a>
+        <a href="/products/{{$product->id}}/edit" class='btn btn-info'>Edit</a> |
+        <a href="/products/{{$product->id}}" class='btn btn-success'>Show</a>
          {!! Form::open(['method' => 'delete', 'action' => ['ProductsController@destroy', $product->id]]) !!}
-          {!! Form::submit('Delete this product ?') !!}
+          {!! Form::submit('Delete this product ?', array('class' => 'btn btn-danger')) !!}
          {!! Form::close() !!}
     </li>
    @endforeach
  </ol>
- <a href="products/create">New product</a>
+ <a href="products/create" class="btn btn-success">New product</a>
 @stop
